97946e76be147a38893adde4a5f102e84db4880b
Sanjaycodes06 <sanjayadash2006@gmail.com>
2026-06-20 21:13:00 +0530

    Fix API URL

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useAuthStore } from "@/store/authStore";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "/api";
+const BASE_URL = "https://naye-pankh-2ffh.onrender.com/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
